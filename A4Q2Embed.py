d973213078b8bdb83d8f9a4331daec3f4c051049
Lora <lora@ualberta.ca>
2021-12-05 11:22:31 -0700

    A4Q2

---
+++ A4Q2Embed.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017')
+
+db = client["A4dbEmbed"]
+
+artist_collection = db["ArtistsTracks"]
+
+out = artist_collection.aggregate([
+    {
+      "$unwind" : "$tracks"
+    },
+    {
+        "$match": {
+        "tracks.track_id": { 
+            "$regex": '^70', 
+            "$options": 'm' 
+            }
+        }
+    },
+    {
+        "$group":{
+            "_id": '',
+            "avg_danceability": {
+                "$avg": "$tracks.danceability"
+            }
+        }
+    },
+])
+                                   
+for x in out:
+    print(x)
---
+++ A4Q2Norm.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017')
+
+db = client["A4dbNorm"]
+
+track_collection = db["Tracks"]
+
+out = track_collection.aggregate([
+    {
+        "$match": {
+        "track_id": { 
+            "$regex": '^70', 
+            "$options": 'm' 
+            }
+        }
+    },
+    {
+        "$group":{
+            "_id": '',
+            "avg_danceability": {
+                "$avg": "$danceability"
+            }
+        }
+    },
+])
+
+for x in out:
+    print(x)
